c9bfeeac51f8ce7ac3c67fbe9136dfa683ab66dd
pmolina-racowireless <pmolina@racowireless.com>
2016-06-20 01:41:50 -0400

    PoSHop : Added SQL scripts

---
+++ DBFSender/SQL/movde.sql
@@ -0,0 +1,44 @@
+﻿USE [PoSHop]
+GO
+
+/****** Object:  Table [dbo].[movde]    Script Date: 6/20/2016 1:40:46 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[movde](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[linea] [int] NULL,
+	[buscar] [varchar](250) NULL,
+	[doc] [varchar](250) NULL,
+	[numero] [int] NULL,
+	[almacen] [varchar](250) NULL,
+	[merc] [int] NULL,
+	[cantidad] [decimal](18, 0) NULL,
+	[frag] [int] NULL,
+	[precio] [decimal](18, 0) NULL,
+	[costo] [decimal](18, 0) NULL,
+	[total] [decimal](18, 0) NULL,
+	[fecha] [datetime] NULL,
+	[flujo] [decimal](18, 0) NULL,
+	[unidad] [varchar](250) NULL,
+	[serial] [varchar](250) NULL,
+	[color] [varchar](250) NULL,
+	[statussync] [varchar](50) NULL,
+ CONSTRAINT [PK_movde] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
---
+++ DBFSender/SQL/movma.sql
@@ -0,0 +1,61 @@
+﻿USE [PoSHop]
+GO
+
+/****** Object:  Table [dbo].[movma]    Script Date: 6/20/2016 1:41:17 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[movma](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[numero] [int] NOT NULL,
+	[doc] [varchar](250) NULL,
+	[Almacen] [varchar](250) NULL,
+	[Cliente] [int] NULL,
+	[fecha] [datetime] NULL,
+	[vence] [datetime] NULL,
+	[vendedor] [varchar](250) NULL,
+	[orden] [int] NULL,
+	[conduce] [int] NULL,
+	[condicion] [int] NULL,
+	[comentario] [varchar](500) NULL,
+	[flujo] [decimal](18, 0) NULL,
+	[entidad] [varchar](250) NULL,
+	[pagares] [decimal](18, 0) NULL,
+	[vencepag] [datetime] NULL,
+	[inicial] [decimal](18, 0) NULL,
+	[fpago] [int] NULL,
+	[descper] [decimal](18, 0) NULL,
+	[descval] [decimal](18, 0) NULL,
+	[itbis] [bit] NULL,
+	[itbisval] [decimal](18, 0) NULL,
+	[bruto] [decimal](18, 0) NULL,
+	[cargo] [decimal](18, 0) NULL,
+	[transferid] [bit] NULL,
+	[transalm] [bit] NULL,
+	[time] [varchar](250) NULL,
+	[impreso] [bit] NULL,
+	[serial] [bit] NULL,
+	[ddoc] [varchar](250) NULL,
+	[dnumero] [int] NULL,
+	[pgdo] [decimal](18, 0) NULL,
+	[otrcargo] [decimal](18, 0) NULL,
+	[ncf] [varchar](250) NULL,
+	[statussync] [varchar](50) NULL,
+ CONSTRAINT [PK_movma_1] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
